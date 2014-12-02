a98a2dfcbbfa37efb124638f022950f372ca5950
Alessandro Desantis <desa.alessandro@gmail.com>
2014-12-02 19:02:58 +0100

    Fix overlapping in payer's name

--- lib/bollettino/renderers/payer_renderer.rb
+++ lib/bollettino/renderers/payer_renderer.rb
@@ -11,7 +11,7 @@ class Bollettino::Renderer::PayerRenderer < Bollettino::Renderer
     write_text(image, [85, 315], payer.name[25..49])
 
     write_text(image, [1508, 375], payer.name[0..22], KERNING_BOX_SMALLEST)
-    write_text(image, [1508, 330], payer.name[22..44], KERNING_BOX_SMALLEST)
+    write_text(image, [1508, 330], payer.name[23..45], KERNING_BOX_SMALLEST)
   end
 
   def self.render_address(image, payer)
